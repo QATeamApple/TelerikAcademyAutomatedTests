f208c4313742642987d2c217a2390d8afd966cb0
Termininja <termininja@gmail.com>
2014-10-16 17:30:04 +0300

    TeamMemberSuggestions classes

---
+++ TestingFramework/QA.TelerikAcademy.Core/Pages/AdminPage/Teamworks/TeamMemberSuggestions/TeamMemberSuggestionsPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QA.TelerikAcademy.Core.Pages;
+using ArtOfTest.WebAii.Core;
+using QA.UI.TestingFramework.Core;
+using QA.TelerikAcademy.Core.Base;
+using QA.UI.TestingFramework.Core.Contracts;
+using QA.UI.TestingFramework.Core.Data;
+
+namespace QA.TelerikAcademy.Core.Pages.AdminPage.Teamwork.TeamMemberSuggestions
+{
+    public class TeamMemberSuggestionsPage
+    {
+        public readonly string TeamMemberSuggestionsUrl = @"http://test.telerikacademy.com/Administration_Teamwork/TeamMemberSuggestions";
+
+        public TeamMemberSuggestionsPageMap Map
+        {
+            get
+            {
+                return new TeamMemberSuggestionsPageMap();
+            }
+        }
+
+        public void ResetTeamwork()
+        {
+           //TODO
+        }
+    }
+}
---
+++ TestingFramework/QA.TelerikAcademy.Core/Pages/AdminPage/Teamworks/TeamMemberSuggestions/TeamMemberSuggestionsPageMap.cs
@@ -0,0 +1,12 @@
+﻿namespace QA.TelerikAcademy.Core.Pages.AdminPage.Teamwork.TeamMemberSuggestions
+{
+    using System;
+    using ArtOfTest.WebAii.Controls.HtmlControls;
+    using QA.UI.TestingFramework.Core;
+    using QA.UI.TestingFramework.Core.Contracts;
+
+    public class TeamMemberSuggestionsPageMap : BaseElementMap
+    {
+
+    }
+}
