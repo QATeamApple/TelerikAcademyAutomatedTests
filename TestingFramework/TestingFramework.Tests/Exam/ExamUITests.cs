3cd4effd31e1dc6d583dce0705e93c69961dce86
ms-nenova <ms.nenova@gmail.com>
2014-10-16 13:05:25 +0300

    Exam UI tests added

--- TestingFramework/TestingFramework.Tests/Exam/ExamUITests.cs
+++ TestingFramework/TestingFramework.Tests/Exam/ExamUITests.cs
@@ -10,9 +10,67 @@
     using QA.TelerikAcademy.Core.Base;
     using QA.UI.TestingFramework.Core;
     using QA.UI.TestingFramework.Core.Data;
+    using QA.TelerikAcademy.Core.Pages.UI.Exams;
 
     public class ExamUITests : BaseTest
     {
-       
+        private User currentUser;
+
+        public ExamPage ExamPage { get; set; }
+        public ExamPageValidator ExamPageValidator { get; set; }
+
+        public override void TestInit()
+        {
+            this.ExamPage = new ExamPage();
+            this.ExamPageValidator = new ExamPageValidator();
+
+            this.currentUser = new User()
+            {
+                Email = "ninja@ninja.com",
+                Username = "Ninja",
+                Password = "123456",
+                FirstName = "FirstName",
+                LastName = "LastName"
+            };
+
+            AcademyLoginProvider.Instance.LoginUser(currentUser);
+        }
+
+        public override void TestCleanUp()
+        {
+            AcademyLoginProvider.Logout();
+        }
+
+        [TestMethod]
+        public void UploadExam()
+        {
+            this.ExamPage.Navigate();
+            this.ExamPage.UploadExam();
+            this.ExamPageValidator.UploadExam();
+        }
+
+        [TestMethod]
+        public void UploadExamWithNoFile()
+        {
+            this.ExamPage.Navigate();
+            this.ExamPage.UploadExamWithNoFile();
+            this.ExamPageValidator.UploadExamWithNoFile();
+        }
+
+        [TestMethod]
+        public void UploadExamWithUnsupportedFormat()
+        {
+            this.ExamPage.Navigate();
+            this.ExamPage.UploadExamWithUnsupportedFormat();
+            this.ExamPageValidator.UploadExamWithUnsupportedFormat();
+        }
+
+        [TestMethod]
+        public void UploadExamWithBigFile()
+        {
+            this.ExamPage.Navigate();
+            this.ExamPage.UploadExamkWithBigSize();
+            this.ExamPageValidator.UploadExamWithBigSize();
+        }
     }
 }
