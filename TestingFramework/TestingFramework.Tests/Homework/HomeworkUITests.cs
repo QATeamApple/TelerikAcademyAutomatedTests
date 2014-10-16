3578eb8a2bf3cef0564a4bc8c77596a631ac7b2a
kirilt7 <kirilt7@gmail.com>
2014-10-16 11:13:52 +0300

    UI Homework test added

--- TestingFramework/TestingFramework.Tests/Homework/HomeworkUITests.cs
+++ TestingFramework/TestingFramework.Tests/Homework/HomeworkUITests.cs
@@ -10,9 +10,72 @@
     using QA.TelerikAcademy.Core.Base;
     using QA.UI.TestingFramework.Core;
     using QA.UI.TestingFramework.Core.Data;
+    using QA.TelerikAcademy.Core.Pages.UI.Homework;
 
+    [TestClass]
     public class HomeworkUITests : BaseTest
     {
-       
+        private User currentUser;
+
+        public HomeworkPage HomeworkPage { get; set; }
+        public HomeworkPageValidator HomeworkPageValidator { get; set; }
+
+        public override void TestInit()
+        {
+            this.HomeworkPage = new HomeworkPage();
+            this.HomeworkPageValidator = new HomeworkPageValidator();
+
+            this.currentUser = new User()
+            {
+                Email = "ninja@ninja.com",
+                Username = "Ninja",
+                Password = "123456",
+                FirstName = "FirstName",
+                LastName = "LastName"
+            };
+
+            AcademyLoginProvider.Instance.LoginUser(currentUser);
+        }
+
+        public override void TestCleanUp()
+        {
+            AcademyLoginProvider.Logout();
+        }
+
+        [TestMethod]
+        public void UploadHomework()
+        {
+            this.HomeworkPage.Navigate();
+            this.HomeworkPage.UploadHomework();
+            this.HomeworkPageValidator.UploadHomework();
+            this.HomeworkPage.DeleteHomework(this.currentUser);
+        }
+
+        [TestMethod]
+        public void UploadHomeworkWithNoFile()
+        {
+            this.HomeworkPage.Navigate();
+            this.HomeworkPage.UploadHomeworkWithNoFile();
+            this.HomeworkPageValidator.UploadHomeworkWithNoFile();
+            this.HomeworkPage.DeleteHomework(this.currentUser);
+        }
+
+        [TestMethod]
+        public void UploadHomeworkWithUnsupportedFormat()
+        {
+            this.HomeworkPage.Navigate();
+            this.HomeworkPage.UploadHomeworkWithUnsupportedFormat();
+            this.HomeworkPageValidator.UploadHomeworkWithUnsupportedFormat();
+            this.HomeworkPage.DeleteHomework(this.currentUser);
+        }
+
+        [TestMethod]
+        public void UploadHomeworkWithBigFile()
+        {
+            this.HomeworkPage.Navigate();
+            this.HomeworkPage.UploadHomeworkWithBigSize();
+            this.HomeworkPageValidator.UploadHomeworkWithBigSize();
+            this.HomeworkPage.DeleteHomework(this.currentUser);
+        }
     }
 }
