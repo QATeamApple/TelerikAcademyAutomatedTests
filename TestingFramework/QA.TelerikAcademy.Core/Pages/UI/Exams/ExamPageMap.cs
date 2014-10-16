96437c55509524729856662c67c6ca93ea43aed7
ms-nenova <ms.nenova@gmail.com>
2014-10-16 12:21:02 +0300

    UI exam pages

---
+++ TestingFramework/QA.TelerikAcademy.Core/Pages/UI/Exams/ExamPageMap.cs
@@ -0,0 +1,47 @@
+﻿using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Core;
+using QA.UI.TestingFramework.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA.TelerikAcademy.Core.Pages.UI.Exams
+{
+    public class ExamPageMap : BaseElementMap
+    {
+        public HtmlInputFile SelectFile
+        {
+            get
+            {
+                return this.Find.ById<HtmlInputFile>("examFile");
+            }
+        }
+
+        public HtmlInputSubmit Upload
+        {
+            get
+            {
+                return this.Find.ByAttributes<HtmlInputSubmit>("value=Качете файл");
+            }
+        }
+
+        public HtmlDiv ValidationError
+        {
+            get
+            {
+                return this.Find.ByAttributes<HtmlDiv>("class=validation-summary-errors");
+            }
+        }
+
+        public HtmlControl UploadSuccessConfirmationMessage
+        {
+            get
+            {
+                return this.Find.ByContent<HtmlDiv>("Файлът ви е изпратен успешно!");
+            }
+        }
+
+    }
+}
---
+++ TestingFramework/QA.TelerikAcademy.Core/Pages/UI/Exams/ExamPageValidator.cs
@@ -0,0 +1,36 @@
+﻿namespace QA.TelerikAcademy.Core.Pages.UI.Exams
+{
+    using QA.UI.TestingFramework.Core;
+    using QA.UI.TestingFramework.Core.Data;
+
+    public class ExamPageValidator
+    {
+        public ExamPageMap Map
+        {
+            get
+            {
+                return new ExamPageMap();
+            }
+        }
+
+        public void UploadExam()
+        {
+            this.Map.UploadSuccessConfirmationMessage.AssertTextEquals("Файлът ви е изпратен успешно!");
+        }
+
+        public void UploadExamWithNoFile()
+        {
+            this.Map.ValidationError.AssertTextContains("Не сте прикачили файл!");
+        }
+
+        public void UploadExamWithUnsupportedFormat()
+        {
+            this.Map.ValidationError.AssertTextContains("Невалиден формат на файла!");
+        }
+
+        public void UploadExamWithBigSize()
+        {
+            this.Map.ValidationError.AssertTextContains("Размерът на файла ви е по-голям от разрешения лимит от 32 MB");
+        }
+    }
+}
