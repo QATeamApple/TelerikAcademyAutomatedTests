5395e66c9e19e8de3e92eed23f90cda2b14e1b4d
ms-nenova <ms.nenova@gmail.com>
2014-10-29 23:08:29 +0200

    Exam and Test Sign Up pages

---
+++ TestingFramework/QA.TelerikAcademy.Core/Pages/UIPages/Exams/ExamSignUpPage/ExamSignUpPage.cs
@@ -0,0 +1,38 @@
+﻿namespace QA.TelerikAcademy.Core.Pages.UIPages.Exams.ExamSignUpPage
+{
+    using System.Windows.Forms;
+    using ArtOfTest.WebAii.Core;
+    using QA.TelerikAcademy.Core.Pages.UIPages.Exams.ExamSignUpPage;
+
+    public class ExamSignUpPage
+    {
+        public readonly string Url = @"http://test.telerikacademy.com/Courses/PracticalExams/Select/22";
+
+        public ExamSignUpPageMap Map
+        {
+            get
+            {
+                return new ExamSignUpPageMap();
+            }
+        }
+
+        public ExamSignUpPageValidator Validator
+        {
+            get
+            {
+                return new ExamSignUpPageValidator();
+            }
+        }
+
+        public void Navigate()
+        {
+            Manager.Current.ActiveBrowser.NavigateTo(this.Url);
+        }
+
+        public void SignUpForAnExam()
+        {
+            this.Map.SignUp.Click();
+            Manager.Current.Desktop.KeyBoard.KeyPress(Keys.Enter);
+        }
+    }
+}
