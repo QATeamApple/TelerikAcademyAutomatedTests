f45200479da28008f3579cb1a2bbe9023db2bb52
Termininja <termininja@gmail.com>
2014-10-29 20:21:11 +0200

    New test structure

---
+++ TestingFramework/QA.TelerikAcademy.Tests/AdministrationPanelTests/TeamworkModulesTests/TeamMemberSuggestionsTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA.TelerikAcademy.Tests.Administration_panel.Teamwork_modules
+{
+    class TeamMemberSuggestionsTests
+    {
+    }
+}
