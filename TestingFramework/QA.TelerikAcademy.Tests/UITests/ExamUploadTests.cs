26e6340a67b36a68d52ea42505a87608fc2e7de5
Termininja <termininja@gmail.com>
2014-10-30 16:53:46 +0200

    Refactoring

--- TestingFramework/QA.TelerikAcademy.Tests/UITests/ExamUploadTests.cs
+++ TestingFramework/QA.TelerikAcademy.Tests/UITests/ExamUploadTests.cs
@@ -2,20 +2,35 @@
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using QA.TelerikAcademy.Core.Base;
+    using QA.TelerikAcademy.Core.Pages.UIPages.Exams.DownloadLatestExamVersion;
+    using QA.TelerikAcademy.Core.Pages.UIPages.Exams.ExamReUploadPage;
     using QA.TelerikAcademy.Core.Pages.UIPages.Exams.ExamUploadPage;
     using QA.UI.TestingFramework.Core;
-    
+
     [TestClass]
     public class ExamUploadTests : BaseTest
     {
         public ExamUploadPage ExamUploadPage { get; set; }
 
+        public ExamReUploadPage ExamReUploadPage { get; set; }
+
+        public DownloadLatestExamVersionPage DownloadLatestExamVersionPage { get; set; }
+
         public ExamUploadPageValidator ExamUploadPageValidator { get; set; }
+
+        public ExamReUploadPageValidator ExamReUploadPageValidator { get; set; }
+
+        public DownloadLatestExamVersionPageValidator DownloadLatestExamVersionPageValidator { get; set; }
 
         public override void TestInit()
         {
             this.ExamUploadPage = new ExamUploadPage();
+            this.ExamReUploadPage = new ExamReUploadPage();
             this.ExamUploadPageValidator = new ExamUploadPageValidator();
+            this.ExamReUploadPageValidator = new ExamReUploadPageValidator();
+            this.DownloadLatestExamVersionPage = new DownloadLatestExamVersionPage();
+            this.DownloadLatestExamVersionPageValidator = new DownloadLatestExamVersionPageValidator();
+
             base.TestInit();
         }
 
@@ -25,6 +40,7 @@
         }
 
         [Owner("Vanina Nenova")]
+        [Priority(1)]
         [TestMethod]
         public void UploadExam()
         {
@@ -34,6 +50,7 @@
         }
 
         [Owner("Vanina Nenova")]
+        [Priority(1)]
         [TestMethod]
         public void UploadExamWithNoFile()
         {
@@ -43,6 +60,7 @@
         }
 
         [Owner("Vanina Nenova")]
+        [Priority(1)]
         [TestMethod]
         public void UploadExamWithUnsupportedFormat()
         {
@@ -52,12 +70,64 @@
         }
 
         [Owner("Vanina Nenova")]
+        [Priority(1)]
         [TestMethod]
         public void UploadExamWithBigFile()
         {
             this.ExamUploadPage.Navigate();
             this.ExamUploadPage.UploadExamkWithBigSize(this.TestFileBigPath);
             this.ExamUploadPageValidator.UploadExamWithBigSize();
+        }
+
+        [Owner("Vanina Nenova")]
+        [Priority(1)]
+        [TestMethod]
+        public void ReUploadExam()
+        {
+            this.ExamReUploadPage.Navigate();
+            this.ExamReUploadPage.ReUploadExam(this.TestFilePath);
+            this.ExamReUploadPageValidator.ReUploadExam();
+        }
+
+        [Owner("Vanina Nenova")]
+        [Priority(1)]
+        [TestMethod]
+        public void ReUploadExamWithNoFile()
+        {
+            this.ExamReUploadPage.Navigate();
+            this.ExamReUploadPage.ReUploadExamWithNoFile();
+            this.ExamReUploadPageValidator.ReUploadExamWithNoFile();
+        }
+
+        [Owner("Vanina Nenova")]
+        [Priority(1)]
+        [TestMethod]
+        public void ReUploadExamWithUnsupportedFormat()
+        {
+            this.ExamReUploadPage.Navigate();
+            this.ExamReUploadPage.ReUploadExamWithUnsupportedFormat(this.TestFileUnsupportedPath);
+            this.ExamReUploadPageValidator.ReUploadExamWithUnsupportedFormat();
+        }
+
+        [Owner("Vanina Nenova")]
+        [Priority(1)]
+        [TestMethod]
+        public void ReUploadExamWithBigFile()
+        {
+            this.ExamReUploadPage.Navigate();
+            this.ExamReUploadPage.ReUploadExamkWithBigSize(this.TestFileBigPath);
+            this.ExamReUploadPageValidator.ReUploadExamWithBigSize();
+        }
+
+        [Owner("Vanina Nenova")]
+        [Priority(2)]
+        [TestMethod]
+        public void DownloadLatestExamVersion()
+        {
+            this.DownloadLatestExamVersionPage.Navigate();
+            this.DownloadLatestExamVersionPage.UploadExam(this.TestFilePath);
+            this.DownloadLatestExamVersionPageValidator.UploadExam();
+            this.DownloadLatestExamVersionPage.DownloadLatestExam();
         }
     }
 }
