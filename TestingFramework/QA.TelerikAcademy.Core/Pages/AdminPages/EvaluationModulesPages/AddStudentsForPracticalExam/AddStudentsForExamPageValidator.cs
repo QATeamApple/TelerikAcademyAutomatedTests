2f8d5930e41b2862a508589b20613ec854667a7f
tinskiwe <tinskiwe@gmail.com>
2014-10-30 16:00:05 +0200

    Added tests for "Add Students for Practical Exam" module

---
+++ TestingFramework/QA.TelerikAcademy.Core/Pages/AdminPages/EvaluationModulesPages/AddStudentsForPracticalExam/AddStudentsForExamPageValidator.cs
@@ -0,0 +1,65 @@
+﻿namespace QA.TelerikAcademy.Core.Pages.AdminPages.EvaluationModulesPages.AddStudentsForPracticalExam
+{
+    using QA.UI.TestingFramework.Core;
+
+    public class AddStudentsForExamPageValidator
+    {
+        public AddStudentsForExamPageMap Map
+        {
+            get
+            {
+                return new AddStudentsForExamPageMap();
+            }
+        }
+
+        public void StudentsAddedToExamsSuccessConfirmationMessage()
+        {
+            this.Map.StudentsAddedToExamsSuccessConfirmationMessage.AssertIsPresent();
+        }
+
+        public void CourseErrorMessage()
+        {
+            this.Map.StudentNotInCourseErrorMessage.AssertTextContains("не е записан за курса, в чийто практически изпит искате да го запишете");
+        }
+
+        public void InvalidCourseErrorMessage()
+        {
+            this.Map.InvalidFileNameErrorMessage.AssertIsPresent();
+        }
+
+        public void PleaseChooseColumnMessage()
+        {
+            this.Map.PleaseChooseColumnMessage.AssertIsPresent();
+        }
+
+        public void PleaseChooseFileMessage()
+        {
+            this.Map.PleaseChooseFileMessage.AssertIsPresent();
+        }
+
+        public void PleaseChooseExamMessage()
+        {
+            this.Map.PleaseChooseExamMessage.AssertIsPresent();
+        }
+
+        public void PleaseChooseCriteriaMessage()
+        {
+            this.Map.PleaseChooseCriteriaMessage.AssertIsPresent();
+        }
+
+        public void InvalidFileNameErrorMessage()
+        {
+            this.Map.InvalidFileNameErrorMessage.AssertIsPresent();
+        }
+
+        public void InvalidSuccessMessage()
+        {
+            this.Map.StudentsAddedToExamsSuccessConfirmationMessage.AssertIsNull();
+        }
+
+        public void InvalidDataFormatErrorMessage()
+        {
+            this.Map.InvalidDataFormatErrorMessage.AssertIsPresent();
+        }
+    }
+}
