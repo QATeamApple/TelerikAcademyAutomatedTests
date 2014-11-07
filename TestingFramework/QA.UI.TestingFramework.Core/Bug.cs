a1ec94c0a858485e22c3171f48fd46a222b170db
Termininja <termininja@gmail.com>
2014-11-07 16:37:44 +0200

    Bug Attribute

---
+++ TestingFramework/QA.UI.TestingFramework.Core/Bug.cs
@@ -0,0 +1,15 @@
+﻿namespace QA.UI.TestingFramework.Core
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Struct)]
+    public class Bug : Attribute
+    {
+        private int id;
+
+        public Bug(int bigId)
+        {
+            this.id = bigId;
+        }
+    }
+}
