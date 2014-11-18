831715a62ddffa0cad830a65e1125091e1f1012e
tinskiwe <tinskiwe@gmail.com>
2014-11-18 02:46:24 +0200

    Added tests for Practical Exam Evaluation module

--- TestingFramework/QA.TelerikAcademy.Tests/Admin/EvaluationModules/PracticalExamEvaluationsTests.cs
+++ TestingFramework/QA.TelerikAcademy.Tests/Admin/EvaluationModules/PracticalExamEvaluationsTests.cs
@@ -2,10 +2,38 @@
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using QA.TelerikAcademy.Core.Base;
+    using QA.TelerikAcademy.Core.Pages.Admin.EvaluationModules.PracticalExamEvaluations;
+    using QA.UI.TestingFramework.Core;
+    using QA.UI.TestingFramework.Core.Data;
+    using System.IO;
+    using System.Threading;
 
     [TestClass]
     public class PracticalExamEvaluationsTests : AcademyBaseTest
     {
+        public PracticalExamEvaluationsPage PracticalExamEvaluationsPage { get; set; }
+
+        public override void TestInit()
+        {
+            this.PracticalExamEvaluationsPage = new PracticalExamEvaluationsPage();
+
+            base.TestInit();
+        }
+
+        [Owner("Svetlin Nyagolov")]
+        [Priority(3)]
+        [TestMethod]
+        public void EditPracticalExamEvaluation()
+        {
+            this.PracticalExamEvaluationsPage.Navigate();
+            Thread.Sleep(1500);
+            this.PracticalExamEvaluationsPage.EvaluatedBy("tinskiwe");
+            Thread.Sleep(1000);
+
+            this.PracticalExamEvaluationsPage.UpdateSettings();
+        }
+
+
 
     }
 }
